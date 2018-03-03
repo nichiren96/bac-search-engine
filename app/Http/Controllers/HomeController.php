9fe347fd62bb3dcb0e4c54244cc29f88308d6559
Roel Tombozafy <rtombozafy96@gmail.com>
2018-03-03 14:08:51 +0300

    Premier commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nichiren
+ * Date: 3/3/18
+ * Time: 1:52 PM
+ */
+
+namespace App\Http\Controllers;
+
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return 'Hello World';
+    }
+}
